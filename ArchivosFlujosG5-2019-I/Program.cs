90ded47bb1c23c9d5dd0a06c7a17f7f07687188f
faraday58 <jarodriguezvera@gmail.com>
2018-11-05 13:14:25 -0600

    Ejemplo

--- ArchivosFlujosG5-2019-I/Program.cs
+++ ArchivosFlujosG5-2019-I/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ArchivosFlujosG5_2019_I
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            StreamWriter sw = null;
+            StreamReader sr=null;
+            FileStream fs= null;
+            try
+            {
+                fs = new FileStream("Archivo1.txt", FileMode.Append, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                Console.WriteLine("Escribe una calaverita \n" +
+                    "Para finalizar una línea pulse <Enter> \n" +
+                    "Para finalizar el documento pulse dos veces <Enter>");
+
+                string aux = Console.ReadLine();
+                while( aux.Length != 0)
+                {
+                    sw.WriteLine(aux);
+                    aux = Console.ReadLine();
+                }
+
+
+            }
+            catch(IOException error)
+            {
+                Console.WriteLine("Error: " + error.Message);
+            }
+            finally
+            {
+               
+                sw.Close();
+                fs.Close();
+            }
+
+            try
+            {
+                fs = new FileStream("Archivo1.txt", FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(fs);
+                string aux = sr.ReadLine();
+                while(aux != null)
+                {
+                    Console.WriteLine(aux);
+                    aux=sr.ReadLine();
+                }
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine("Error: "+error.Message);
+            }
+            finally
+            {
+                fs.Close();
+                sr.Close();
+            }
+
         }
     }
 }
---
+++ FormularioArchivos/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FormularioArchivos
+{
+    public partial class Form1 : Form
+    {
+        FileStream fs;
+        StreamReader sr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if( ofd.ShowDialog() == DialogResult.OK  )
+            {
+                try
+                {
+                    fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    sr = new StreamReader(fs);
+                    string aux = sr.ReadLine();
+                    while (aux != null)
+                    {
+
+                        rtbNotas.AppendText(aux + "\n");
+                        aux = sr.ReadLine();
+                    }
+                }
+                catch(IOException error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+                finally
+                {
+                    sr.Close();
+                    fs.Close();
+                }
+            }
+
+        }
+    }
+}
